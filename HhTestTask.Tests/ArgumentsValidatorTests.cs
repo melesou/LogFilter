95cd816d7334b39e097fc2fb4cbc7eacd19c01fc
melesou <melesoyrus@gmail.com>
2024-04-06 15:17:26 +0500

    Слега доработаны тесты

--- HhTestTask.Tests/ArgumentsValidatorTests.cs
+++ HhTestTask.Tests/ArgumentsValidatorTests.cs
@@ -4,6 +4,21 @@ namespace HhTestTask.Tests;
 
 public class ArgumentsValidatorTests
 {
+    private string _inputFilePath = "";
+    private string _outputFilePath = "";
+
+    [SetUp]
+    public void Setup()
+    {
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        _inputFilePath = Path.Combine(baseDirectory, "..", "..", "..", "LogInput.txt");
+        _inputFilePath = Path.GetFullPath(_inputFilePath);
+
+        _outputFilePath = Path.Combine(baseDirectory, "..", "..", "..", "LogOutput.txt");
+        _outputFilePath = Path.GetFullPath(_outputFilePath);
+    }
+
     [Test]
     public void Validate_Test_InvalidArguments()
     {
@@ -24,7 +39,7 @@ public class ArgumentsValidatorTests
     public void Validate_Test_LackOfArgumentsPairs()
     {
         var txtPath = Directory.GetCurrentDirectory();
-        var args = new[] { "--file-log", "--file-output", $"{txtPath}\\LogOutput.txt", "--time-start", "2023-01-01", "--time-end ", "2024-04-08", "--address-start", "0.0.0.0", "--address-mask", "255.255.255.255" };
+        var args = new[] { "--file-log", "--file-output", _inputFilePath, "--time-start", "2023-01-01", "--time-end ", "2024-04-08", "--address-start", "0.0.0.0", "--address-mask", "255.255.255.255" };
 
         Assert.IsFalse(ArgumentsValidator.Validate(args));
     }
@@ -41,7 +56,7 @@ public class ArgumentsValidatorTests
     public void Validate_Test_WithoutOptionalParameters()
     {
         var txtPath = Directory.GetCurrentDirectory();
-        var args = new[] { "--file-log", $"{txtPath}\\LogInput.txt", "--file-output", $"{txtPath}\\LogOutput.txt", "--time-start", "2023-01-01", "--time-end", "2024-04-08" };
+        var args = new[] { "--file-log", _inputFilePath, "--file-output", _outputFilePath, "--time-start", "2023-01-01", "--time-end", "2024-04-08" };
 
         Assert.True(ArgumentsValidator.Validate(args));
     }
@@ -50,7 +65,7 @@ public class ArgumentsValidatorTests
     public void Validate_Test_AllGood()
     {
         var txtPath = Directory.GetCurrentDirectory();
-        var args = new[] { "--file-log", $"{txtPath}\\LogInput.txt", "--file-output", $"{txtPath}\\LogOutput.txt", "--time-start", "2023-01-01", "--time-end", "2024-04-08", "--address-start", "0.0.0.0", "--address-mask", "255.255.255.255" };
+        var args = new[] { "--file-log", _inputFilePath, "--file-output", _outputFilePath, "--time-start", "2023-01-01", "--time-end", "2024-04-08", "--address-start", "0.0.0.0", "--address-mask", "255.255.255.255" };
 
         Assert.True(ArgumentsValidator.Validate(args));
     }
